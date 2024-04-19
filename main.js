0285664a6133dc5c7d07156ca372ca5e06636530
Álvaro Gomes <107187145+alvarog1A@users.noreply.github.com>
2024-04-19 09:54:50 -0300

    eee

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual os melhor time?");
+    var respostaTime = prompt("Qual é o apelido do Álvaro?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
+    if (respostaTime.toLowerCase() === "Bang") {
       alert("Isso mesmo! O Athlético PR é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
